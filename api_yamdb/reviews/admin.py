c6728fe4142f35b62fa88de3b480e4d7ae5fc7e7
Evgeniy Volochek <volochek93@yandex.ru>
2022-10-31 23:30:21 +0400

    Добавлена админка отзывов

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,2 +1,17 @@
 from django.contrib import admin
 
+from .models import Review
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'title',
+        'text',
+        'author',
+        'score',
+    )
+    search_fields = ('title', 'author', 'pub_date',)
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'
