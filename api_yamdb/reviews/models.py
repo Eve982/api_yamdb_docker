acbe842a80a295f2e4ff5154c790e80cfd1035f2
Evgeniy Volochek <volochek93@yandex.ru>
2022-10-31 23:29:38 +0400

    Создана модель отзывов

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class Review(models.Model):
+    """Модель отзывов на произведение."""
+
+    title = models.ForeignKey(
+        'Title',
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='произведение'
+    )
+    text = models.CharField(
+        'Текст отзыва',
+        max_length=200
+    )
+    author = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Автор'
+    )
+    score = models.IntegerField(
+        'Оценка',
+        validators=(
+            MinValueValidator(1),
+            MaxValueValidator(10)
+        ),
+        error_messages={
+            'validators': 'Оценка от 1 до 10!'
+        }
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации отзыва',
+        auto_now_add=True,
+        db_index=True
+    )
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+        constraints = [
+            models.UniqueConstraint(
+                fields=('title', 'author',),
+                name='unique_review'
+            )
+        ]
+        ordering = ('-pub_date',)
+
+    def __str__(self):
+        return self.text
