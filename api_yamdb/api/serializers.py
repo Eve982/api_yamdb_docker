316e4a0bc0d19da7ee7d68074145679327efae7a
Evgeniy Volochek <volochek93@yandex.ru>
2022-11-01 00:22:57 +0400

    создал заглушки

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,7 +1,39 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 
-from reviews.models import Review
+from reviews.models import Category, Comment, Genre, Review, Title, User
+
+
+class UsersSerializer(serializers.ModelSerializer):
+    pass
+
+
+class NotAdminSerializer(serializers.ModelSerializer):
+    pass
+
+
+class GetTokenSerializer(serializers.ModelSerializer):
+    pass
+
+
+class SignUpSerializer(serializers.ModelSerializer):
+    pass
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    pass
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    pass
+
+
+class TitleReadSerializer(serializers.ModelSerializer):
+    pass
+
+
+class TitleWriteSerializer(serializers.ModelSerializer):
+    pass
 
 
 class ReviewSerializer(serializers.ModelSerializer):
@@ -27,10 +59,14 @@ class ReviewSerializer(serializers.ModelSerializer):
     def validate(self, data):
         request = self.context.get('request')
         title_id = self.context.get('view').kwargs.get('title_id')
-        title = get_object_or_404('Title', pk=title_id)
+        title = get_object_or_404(Title, pk=title_id)
         if (
             request.method == 'POST'
             and Review.objects.filter(author=request.user, title=title).exists()
         ):
             raise serializers.ValidationError('Вы уже оставили отзыв!')
         return data
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    pass
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,12 +1,29 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+class User(AbstractUser):
+    pass
+
+
+class Category(models.Model):
+    pass
+
+
+class Genre(models.Model):
+    pass
+
+
+class Title(models.Model):
+    pass
 
 
 class Review(models.Model):
     """Модель отзывов на произведение."""
 
     title = models.ForeignKey(
-        'Title',
+        Title,
         on_delete=models.CASCADE,
         related_name='reviews',
         verbose_name='произведение'
@@ -16,7 +33,7 @@ class Review(models.Model):
         max_length=200
     )
     author = models.ForeignKey(
-        'User',
+        User,
         on_delete=models.CASCADE,
         related_name='reviews',
         verbose_name='Автор'
@@ -50,3 +67,7 @@ class Review(models.Model):
 
     def __str__(self):
         return self.text
+
+
+class Comment(models.Model):
+    pass
