5ac1b9af170a57eaf246782f6382330c8e12756a
Евгений Волочек <105015075+EvgVol@users.noreply.github.com>
2022-11-01 09:38:28 +0300

    Прописал заглушки для views

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,2 +1,33 @@
-from django.db import models
+from rest_framework import viewsets
 
+from .serializers import (CategorySerializer, CommentSerializer,
+                          GenreSerializer, GetTokenSerializer,
+                          NotAdminSerializer, ReviewSerializer,
+                          SignUpSerializer, TitleReadSerializer,
+                          TitleWriteSerializer, UsersSerializer)
+from .mixins import CreateListViewSet
+from reviews.models import Category, Genre, Review, Title
+
+
+class UsersViewSet(viewsets.ModelViewSet):
+    pass
+
+
+class CategoryViewSet(CreateListViewSet):
+    pass
+
+
+class GenreViewSet(CreateListViewSet):
+    pass
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    pass
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    pass
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    pass
