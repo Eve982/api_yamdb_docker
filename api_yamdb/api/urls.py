7b65eb3e5fed04fe75efb5e91032940ece894947
Михаил Дудин <dude-inn@yandex.ru>
2022-11-02 21:10:29 +0800

    ИсправлеДобавлены url's для категорий, жанров, произведений

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -7,6 +7,9 @@ from .views import (CategoryViewSet, CommentViewSet, GenreViewSet,
 
 router_v1 = routers.DefaultRouter()
 
+router_v1.register(r'categories', CategoryViewSet)
+router_v1.register(r'genres', GenreViewSet)
+router_v1.register(r'titles', TitleViewSet)
 router_v1.register(
     r'titles/(?P<title_id>\d+)/reviews',
     ReviewViewSet, basename='reviews'
