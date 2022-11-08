3a464c3c9c948971d3e7e3e7a15d03cf02a79eb9
Evgeniy Volochek <volochek93@yandex.ru>
2022-11-08 23:27:30 +0400

    Исправлен class SingUpSerializer(serializers.ModelSerializer)

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -9,16 +9,39 @@ from reviews.models import (Comment, Review,
 class SingUpSerializer(serializers.ModelSerializer):
     """Сериализатор для регистрации."""
 
+    email = serializers.EmailField(required=True)
+    username = serializers.CharField(required=True)
+
     class Meta:
         model = User
         fields = ('username', 'email')
 
-    def create(self, validated_data):
-        user = User.objects.create(
-            username=self.validated_data['username'],
-            email=self.validated_data['email'],
-        )
-        return user
+    def validate_username(self, value):
+        if value == 'me' or '':
+            raise serializers.ValidationError(
+                {
+                    'username':
+                    'Нельзя использовать имя me в качестве имени пользователя.'
+                },
+            )
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError(
+                {
+                    'username':
+                    'Пользователь с данным username уже зарегистрирован.'
+                },
+            )
+        return value
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError(
+                {
+                    'email':
+                    'Пользователь с данным email уже зарегистрирован.'
+                },
+            )
+        return value
 
 
 class ConfirmationCodeSerializer(serializers.Serializer):
