d27e927b14d6863481cce9452909c3cfdd774b0e
Евгений Волочек <105015075+EvgVol@users.noreply.github.com>
2022-11-09 07:52:27 +0300

    command

--- api_yamdb/reviews/management/commands/_utils.py
+++ api_yamdb/reviews/management/commands/_utils.py
@@ -1,134 +1,36 @@
 import csv
 import os
 
-from reviews.models import (
-    Category, Genre, Title, Review, Comment, User
-)
 from django.conf import settings
+from django.core.management import BaseCommand
+
+from reviews.models import (Category, Genre, Title,
+                            Review, Comment, User)
 
 
-FILE_DIR = os.path.join(settings.BASE_DIR, r"static\data")
+DATA = {
+    User: 'users.csv',
+    Category: 'category.csv',
+    Genre: 'genre.csv',
+    Title: 'titles.csv',
+    Review: 'review.csv',
+    Comment: 'comments.csv',
+}
 
+class Command(BaseCommand):
+    """Импортер данных из csv."""
 
-try:
-    def get_data():
-        with open(
-            os.path.join(FILE_DIR, "category.csv"), encoding="utf-8"
-        ) as csvfile:
-            Category.objects.all().delete()
-            reader = csv.DictReader(csvfile, delimiter=",")
-            for row in reader:
-                category = Category(
-                    id=row['id'],
-                    name=row['name'],
-                    slug=row['slug'],
-                )
-                category.save()
-
-        with open(
-            os.path.join(FILE_DIR, "genre.csv"), encoding="utf-8"
-        ) as csvfile:
-            Genre.objects.all().delete()
-            reader = csv.DictReader(csvfile, delimiter=",")
-            for row in reader:
-                genre = Genre(
-                    id=row['id'],
-                    name=row['name'],
-                    slug=row['slug'],
-                )
-                genre.save()
-
-        with open(
-            os.path.join(FILE_DIR, "users.csv"), encoding="utf-8"
-        ) as csvfile:
-            User.objects.all().delete()
-            reader = csv.DictReader(csvfile, delimiter=",")
-            for row in reader:
-                user = User(
-                    id=row['id'],
-                    username=row['username'],
-                    email=row['email'],
-                    role=row['role'],
-                    bio=row['bio'],
-                    first_name=row['first_name'],
-                    last_name=row['last_name'],
-                )
-                user.save()
-
-        with open(
-            os.path.join(FILE_DIR, "titles.csv"), encoding="utf-8"
-        ) as csvfile:
-            Title.objects.all().delete()
-            reader = csv.DictReader(csvfile, delimiter=",")
-            for row in reader:
-                category, created = Category.objects.get_or_create(
-                    id=row['category']
-                )
-                title = Title(
-                    id=row['id'],
-                    name=row['name'],
-                    year=row['year'],
-                    category=category,
-                )
-                title.save()
-
-        with open(
-            os.path.join(FILE_DIR, "genre_title.csv"), encoding="utf-8"
-        ) as csvfile:
-            reader = csv.DictReader(csvfile, delimiter=",")
-            for row in reader:
-                title, created = Title.objects.get_or_create(
-                    id=row['title_id']
-                )
-                genre, created = Genre.objects.get_or_create(
-                    id=row['genre_id']
-                )
-                title.genre.add(genre)
-                title.save()
-
-        with open(
-            os.path.join(FILE_DIR, "review.csv"), encoding="utf-8"
-        ) as csvfile:
-            Review.objects.all().delete()
-            reader = csv.DictReader(csvfile, delimiter=",")
-            for row in reader:
-                title, created = Title.objects.get_or_create(
-                    id=row['title_id']
-                )
-                author, created = User.objects.get_or_create(
-                    id=row['author']
-                )
-                review = Review(
-                    id=row['id'],
-                    title=title,
-                    text=row['text'],
-                    author=author,
-                    score=row['score'],
-                    pub_date=row['pub_date'],
-                )
-                review.save()
-
-        with open(
-            os.path.join(FILE_DIR, "comments.csv"), encoding="utf-8"
-        ) as csvfile:
-            Comment.objects.all().delete()
-            reader = csv.DictReader(csvfile, delimiter=",")
-            for row in reader:
-                review_id, created = Review.objects.get_or_create(
-                    id=row['review_id']
-                )
-                author_id, created = User.objects.get_or_create(
-                    id=row['author']
-                )
-                p = Comment(
-                    review=review_id,
-                    text=row['text'],
-                    author=author_id,
-                    pub_date=row['pub_date'],
-                )
-                p.save()
-except FileNotFoundError as e:
-    raise f"FileNotFoundError successfully handled {e}"
-# except Exception as err:
-#     print(f"Unexpected {err}, {type(err)}")
-#     raise
+    try:
+        def handle(self, *args, **kwargs):
+            for model, csv_f in DATA.items():
+                with open(
+                    f'{settings.BASE_DIR}/static/data/{csv_f}',
+                    'r',
+                    encoding='utf-8'
+                ) as csv_file:
+                    reader = csv.DictReader(csv_file)
+                    model.objects.bulk_create(
+                        model(**data) for data in reader)
+            self.stdout.write(self.style.SUCCESS('Все данные загружены'))
+    except:
+        raise NotImplementedError('Ошибка при выгрузке данных!')
